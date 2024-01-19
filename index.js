397ec18d44baf01ffabbe1891533f988db96e65a
pruutik <x-shimmer-x@live.com>
2024-01-18 17:46:44 -0700

    wip amplitude

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const five = require('johnny-five');
 const { Client, GatewayIntentBits } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
-const { token } = require('./config.json');
+const { OpusEncoder } = require('@discordjs/opus');
+const { voiceChannelId, token } = require('./config.json');
 
 const client = new Client({
 	intents: [
@@ -14,27 +15,57 @@ var led;
 
 var speakers = 0;
 
-const board = new five.Board();
-board.on('ready', () => {
-    led = new five.Led(8);
+// const board = new five.Board();
+// board.on('ready', () => {
+//     led = new five.Led(8);
     client.login(token);
-});
+// });
+
+function calcrms_lin(buffer){
+
+    var rms = 0;
+
+    for(var bufferIndex = 0; bufferIndex < buffer.length; bufferIndex++){
+        rms+= buffer[bufferIndex]*buffer[bufferIndex];
+    }
+
+    rms /= buffer.length;
+    rms = Math.sqrt(rms);
+
+    return rms;
+
+}
 
 client.once('ready', () => {
-    client.channels.fetch('958800820084817923').then((channel) => {
+    client.channels.fetch(voiceChannelId).then((channel) => {
         const connection = joinVoiceChannel({
             channelId: channel.id,
             guildId: channel.guild.id,
             adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false
         });
-        connection.receiver.speaking.on('start', () => {
+        connection.receiver.speaking.on('start', (userId) => {
+            const subcription = connection.receiver.subscribe(userId, { end: { 
+                // behavior: EndBehaviorType.AfterSilence, 
+                duration: 100 
+            }});
+            const encoder = new OpusEncoder(48000,2);
+            subcription.on("data", (audioBuffer) => {
+                // encoder.decode(chunk)
+                // console.log(audioBuffer);
+                let channelData = encoder.decode(audioBuffer)
+                console.log(channelData);
+                // Calculate amplitude
+                const amplitude = calcrms_lin(channelData);
+                console.log(amplitude)
+            })
             speakers++;
-            led.on();
+            // led.on();
         })
         connection.receiver.speaking.on('end', () => {
             speakers--;
             if(speakers<=0){
-                led.off();
+                // led.off();
             }
         })
     });
